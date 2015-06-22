ca080e07157257bec412ba8aa669a61b856f313d
kojiwakayama <koji.wakayama@nearform.com>
2015-06-22 03:03:28 +0200

    Add position to events table and add create multiple events

--- scripts/database/pg/migrations/001.do.init-db.sql
+++ scripts/database/pg/migrations/001.do.init-db.sql
@@ -3,20 +3,20 @@ CREATE TABLE IF NOT EXISTS cd_events
 (
   id character varying NOT NULL,
   name character varying,
-  date timestamp with time zone,
   country json,
   city json,
   address character varying,
-  description character varying,
   capacity integer,
-  public boolean,
-  category character varying,
-  user_types character varying[],
-  dojo_id character varying,
-  status character varying,
-  invites json[],
   created_at timestamp with time zone,
   created_by character varying,
+  date timestamp with time zone,
+  description character varying,
+  dojo_id character varying,
+  invites json[],
+  position json,
+  public boolean,
+  status character varying,
+  user_types character varying[],
   CONSTRAINT pk_cd_events_id PRIMARY KEY (id)
 )
 WITH (
@@ -26,12 +26,12 @@ WITH (
 CREATE TABLE IF NOT EXISTS cd_applications
 (
   id character varying NOT NULL,
-  event_id character varying,
-  user_id character varying,
   name character varying,
-  date_of_birth date,
   attended boolean,
+  date_of_birth date,
+  event_id character varying,
   status character varying,
+  user_id character varying,
   CONSTRAINT pk_cd_applications_id PRIMARY KEY (id)
 )
 WITH (

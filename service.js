50514e633ea77a3d4eaf7b7f7a1b88b453b0ff53
Damian Beresford <dberesford@gmail.com>
2015-07-27 14:47:43 +0100

    Added DOCKER_HOST_IP. Removed localhost() from config

--- service.js
+++ service.js
@@ -17,6 +17,6 @@ seneca.use(require('./es.js'));
 seneca.use(require('./lib/cd-events'));
 
 seneca.listen()
-	.client({type: 'web', host: process.env.TARGETIP || '127.0.0.1', port: 10301, pin: 'role:cd-dojos,cmd:*'})
-	.client({type: 'web', host: process.env.TARGETIP || '127.0.0.1', port: 10303, pin: 'role:cd-users,cmd:*'})
-	.client({type: 'web', host: process.env.TARGETIP || '127.0.0.1', port: 10303, pin: 'role:cd-profiles,cmd:*'});
+	.client({type: 'web', host: process.env.DOCKER_HOST_IP || process.env.TARGETIP || '127.0.0.1', port: 10301, pin: 'role:cd-dojos,cmd:*'})
+	.client({type: 'web', host: process.env.DOCKER_HOST_IP || process.env.TARGETIP || '127.0.0.1', port: 10303, pin: 'role:cd-users,cmd:*'})
+	.client({type: 'web', host: process.env.DOCKER_HOST_IP || process.env.TARGETIP || '127.0.0.1', port: 10303, pin: 'role:cd-profiles,cmd:*'});

a6c13e88801fba5f378c1a9084ced75ca3c39523
Damian Beresford <dberesford@gmail.com>
2015-07-28 10:44:37 +0100

    new relic changes

---
+++ newrelic.js
@@ -0,0 +1,18 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ *
+ * Also see: https://docs.newrelic.com/docs/agents/nodejs-agent/installation-configuration/nodejs-agent-configuration
+ * and: https://github.com/newrelic/node-newrelic/blob/master/lib/config.default.js
+ */
+exports.config = {
+  app_name: ['cp-newrelic-service'],
+  agent_enabled: false, // set via NEW_RELIC_ENABLED for production
+  license_key: '',      // set via NEW_RELIC_LICENSE_KEY
+  logging: {
+    level: 'info',
+    filepath: os.tmpdir() + 'newrelic_agent.log'
+  }
+}

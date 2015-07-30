d140ddfb9aa6eff200f2b3eed91a8144deffaa7e
Damian Beresford <dberesford@gmail.com>
2015-07-30 11:55:08 +0100

    Send seneca debug messages directly to logentries

--- config/config.js
+++ config/config.js
@@ -1,5 +1,46 @@
+var assert = require('assert');
+var LogEntries = require('le_node');
 var path = require('path');
+
 module.exports = function() {
+  function log () {
+    // seneca custom log handlers
+    function debugHandler() {
+      if (process.env.LOGENTRIES_ENABLED === 'true') {
+        assert.ok(process.env.LOGENTRIES_DEBUG_TOKEN, 'No LOGENTRIES_DEBUG_TOKEN set');
+        var le = new LogEntries({
+          token: process.env.LOGENTRIES_DEBUG_TOKEN,
+          flatten: true,
+          flattenArrays: true
+        });
+
+        le.log('debug', arguments);
+      }
+    }
+
+    function errorHandler() {
+      console.error(JSON.stringify(arguments));
+
+      if (process.env.LOGENTRIES_ENABLED === 'true') {
+        assert.ok(process.env.LOGENTRIES_ERRORS_TOKEN, 'No LOGENTRIES_ERROR_TOKEN set');
+        var le = new LogEntries({
+          token: process.env.LOGENTRIES_ERRORS_TOKEN,
+          flatten: true,
+          flattenArrays: true
+        });
+
+        le.log('err', arguments);
+      }
+    }
+
+    return {
+      map:[{
+        level:'debug', handler: debugHandler
+      }, {
+        level:'error', handler: errorHandler
+      }]
+    };
+  };
 
   function pgConfig() {
     return {
@@ -64,6 +105,7 @@ module.exports = function() {
         host: '0.0.0.0',
         port: 10306
       }
-    }
+    },
+    log: log()
   };
 }

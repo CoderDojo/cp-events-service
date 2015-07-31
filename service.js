88e3cd9450faef00b01441d5b8d9ef280dde7964
david-cahill <davidrichardcahill@gmail.com>
2015-07-31 17:32:06 +0100

    remove elasticsearch from events service

--- service.js
+++ service.js
@@ -4,18 +4,12 @@ if (process.env.NEW_RELIC_ENABLED === "true") require('newrelic');
 
 var _ = require('lodash');
 var config = require('./config/config.js')();
-var ESOptions = require('./es-options.js');
-
 var seneca = require('seneca')();
-
 
 seneca.log.info('using config', JSON.stringify(config, null, 4));
 seneca.options(config);
 
-
 seneca.use('postgresql-store', config["postgresql-store"]);
-seneca.use('elasticsearch', _.defaults(config["elasticsearch"], ESOptions));
-seneca.use(require('./es.js'));
 seneca.use(require('./lib/cd-events'));
 
 seneca.listen()

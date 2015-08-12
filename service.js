0405c39cc7f347f4b9cf4e95c485242f99e0fc00
Damian Beresford <dberesford@gmail.com>
2015-08-12 09:29:15 +0100

    Changed to use tcp transport instead of http

--- service.js
+++ service.js
@@ -20,7 +20,7 @@ require('./migrate-psql-db.js')(function (err) {
   console.log("Migrations ok");
 
   seneca.listen()
-	  .client({type: 'web', host: process.env.DOCKER_HOST_IP || process.env.TARGETIP || '127.0.0.1', port: 10301, pin: 'role:cd-dojos,cmd:*'})
-	  .client({type: 'web', host: process.env.DOCKER_HOST_IP || process.env.TARGETIP || '127.0.0.1', port: 10303, pin: 'role:cd-users,cmd:*'})
-	  .client({type: 'web', host: process.env.DOCKER_HOST_IP || process.env.TARGETIP || '127.0.0.1', port: 10303, pin: 'role:cd-profiles,cmd:*'});
+	  .client({type: 'tcp', pin: 'role:cd-dojos,cmd:*'})
+	  .client({type: 'tcp', port: 10303, pin: 'role:cd-users,cmd:*'})
+	  .client({type: 'tcp', port: 10303, pin: 'role:cd-profiles,cmd:*'});
 });

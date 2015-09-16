002999eff50a452eb7924b281e05babb4dcea0b8
Cristian Kiss <cristian.kiss@nearform.com>
2015-09-16 22:12:30 +0300

    reverted changes to start script

--- start.sh
+++ start.sh
@@ -1,13 +1,7 @@
 #! /bin/bash
 
-ENVIRONMENT=$1
+FILE=$1
 START=$2
-
-PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
-FILE="$PROJECT_DIR/config/$ENVIRONMENT.env"
-
-echo $PROJECT_DIR;
-echo $FILE;
 
 USAGE="Usage: ./start.sh <config> <startscript> [startscript_opts]..."
 

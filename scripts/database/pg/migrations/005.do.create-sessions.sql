ef7a6853e1a8583fb13782c120749d0170718f0f
david-cahill <davidrichardcahill@gmail.com>
2015-09-18 15:20:26 +0100

    add session description field to db

--- scripts/database/pg/migrations/005.do.create-sessions.sql
+++ scripts/database/pg/migrations/005.do.create-sessions.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS cd_sessions
 (
   id character varying NOT NULL,
   name character varying,
+  description character varying,
   event_id character varying,
   tickets json[],
   CONSTRAINT pk_cd_sessions PRIMARY KEY (id)

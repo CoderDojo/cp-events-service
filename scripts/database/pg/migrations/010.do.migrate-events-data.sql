cda1e0aeea1cf4527ac5ddbc11e6526b6e8bda0f
david-cahill <davidrichardcahill@gmail.com>
2015-09-22 17:33:07 +0100

    further development of data migration script

--- scripts/database/pg/migrations/010.do.migrate-events-data.sql
+++ scripts/database/pg/migrations/010.do.migrate-events-data.sql
@@ -1,9 +1,14 @@
 DO $$
 	BEGIN
+		CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+		DECLARE
+    		event RECORD;
+    		ticket_string character varying;
 		BEGIN
 			FOR event in SELECT * FROM cd_events
 			LOOP
-				INSERT INTO cd_sessions (name, description, event_id, tickets, status) VALUES ('General', 'General Session', event.id, '{ {"name": "General Admission", "type": "other", "quantity": 40 } }');
+				ticket_string := '{"name": "General Admission", "type": "other", "quantity": "'||event.capacity||'" }';
+				INSERT INTO cd_sessions (id, name, description, event_id, tickets, status) VALUES (uuid_generate_v4(), 'General', 'General Session', event.id, array[ticket_string::json], 'active');
 			END LOOP;
 		END;
 	END;

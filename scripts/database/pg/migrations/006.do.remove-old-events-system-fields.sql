b0e2072ae338f7c28e6a3ba470c4138a6a1c9a69
david-cahill <davidrichardcahill@gmail.com>
2015-09-23 15:15:20 +0100

    remove unused actions

--- scripts/database/pg/migrations/006.do.remove-old-events-system-fields.sql
+++ scripts/database/pg/migrations/006.do.remove-old-events-system-fields.sql
@@ -1,11 +1,11 @@
 DO $$
+  BEGIN
     BEGIN
-    	BEGIN
-			ALTER TABLE cd_events DROP IF EXISTS user_type;
-			ALTER TABLE cd_events DROP IF EXISTS capacity;
-		END;
-		BEGIN
-			DROP TABLE IF EXISTS cd_attendance;
-		END;
-	END;
+      ALTER TABLE cd_events DROP IF EXISTS user_type;
+      ALTER TABLE cd_events DROP IF EXISTS capacity;
+    END;
+    BEGIN
+      DROP TABLE IF EXISTS cd_attendance;
+    END;
+  END;
 $$

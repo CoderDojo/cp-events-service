8b3316dbf0d489526b993f67b3dea63725e89338
ante-sabo <ante@192.168.3.226>
2016-10-26 09:56:17 +0100

    927 added missing  WHEN OTHERS THEN

--- scripts/database/pg/migrations/015.do.add-indexes-to-events.sql
+++ scripts/database/pg/migrations/015.do.add-indexes-to-events.sql
@@ -2,12 +2,12 @@ DO $$
   BEGIN
     BEGIN
         CREATE INDEX events_country ON cd_events((country->>'name'));
-    EXCEPTION
+    EXCEPTION WHEN OTHERS THEN
         RAISE NOTICE 'table cd_events already have index on column country.';
     END;
     BEGIN
 	    CREATE INDEX events_city ON cd_events((city->>'name'));
-    EXCEPTION
+    EXCEPTION WHEN OTHERS THEN
         RAISE NOTICE 'table cd_events already have index on column country.';
     END;
   END;

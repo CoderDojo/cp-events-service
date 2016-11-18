81fc193334792b2ffb51600dfa0fa6032c83d227
Wardormeur <wardormeur@gmail.com>
2016-11-18 18:13:46 +0000

    unify network pinning

--- network.js
+++ network.js
@@ -3,7 +3,7 @@
 module.exports = function (seneca) {
   seneca.listen()
     .client({type: 'web', port: 10305, pin: {role: 'cd-badges', cmd: '*'}})
-    .client({type: 'web', port: 10301, pin: 'role:cd-dojos,cmd:*'})
-    .client({type: 'web', port: 10303, pin: 'role:cd-users,cmd:*'})
-    .client({type: 'web', port: 10303, pin: 'role:cd-profiles,cmd:*'});
+    .client({type: 'web', port: 10301, pin: {role: 'cd-dojos', cmd: '*'}})
+    .client({type: 'web', port: 10303, pin: {role: 'cd-users', cmd: '*'}})
+    .client({type: 'web', port: 10303, pin: {role: 'cd-profiles', cmd: '*'}});
 };

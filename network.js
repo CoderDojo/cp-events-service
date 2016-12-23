51c387805ee11b3f259a5a804db987b94de1ce24
butlerx <cianbutlerx@gmail.com>
2016-12-23 10:44:30 +0000

    add docker ignore

--- network.js
+++ network.js
@@ -2,8 +2,8 @@
 
 module.exports = function (seneca) {
   seneca.listen()
-    .client({type: 'web', port: 10305, pin: {role: 'cd-badges', cmd: '*'}})
-    .client({type: 'web', port: 10301, pin: {role: 'cd-dojos', cmd: '*'}})
-    .client({type: 'web', port: 10303, pin: {role: 'cd-users', cmd: '*'}})
-    .client({type: 'web', port: 10303, pin: {role: 'cd-profiles', cmd: '*'}});
+    .client({type: 'web', host: process.env.CD_BADGES || 'localhost', port: 10305, pin: {role: 'cd-badges', cmd: '*'}})
+    .client({type: 'web', host: process.env.CD_DOJOS || 'localhost', port: 10301, pin: {role: 'cd-dojos', cmd: '*'}})
+    .client({type: 'web', host: process.env.CD_USERS || 'localhost', port: 10303, pin: {role: 'cd-users', cmd: '*'}})
+    .client({type: 'web', host: process.env.CD_USERS || 'localhost', port: 10303, pin: {role: 'cd-profiles', cmd: '*'}});
 };

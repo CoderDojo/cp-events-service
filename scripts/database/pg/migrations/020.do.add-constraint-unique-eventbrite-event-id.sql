b22e3df32a127e57cc3e0dd50527e3c23edf4190
Wardormeur <wardormeur@gmail.com>
2017-11-06 11:26:38 +0000

    Fix condition of error handling catchall bcause of lazyness

--- scripts/database/pg/migrations/020.do.add-constraint-unique-eventbrite-event-id.sql
+++ scripts/database/pg/migrations/020.do.add-constraint-unique-eventbrite-event-id.sql
@@ -3,7 +3,7 @@ DO $$
     BEGIN
         ALTER TABLE cd_events ADD UNIQUE (eventbrite_id);
     EXCEPTION
-        WHEN duplicate_column THEN RAISE NOTICE 'constraint eventbrite_id already exists in cd_events.';
+        WHEN OTHERS THEN RAISE NOTICE 'constraint eventbrite_id already exists in cd_events.';
     END;
   END;
 $$

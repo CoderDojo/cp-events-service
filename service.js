a0f1af63abbf82ea91ce19ffc77b3940e918f4ca
butlerx <butlerx@notthe.cloud>
2017-11-15 11:12:56 +0000

    make production logs less noicy

--- service.js
+++ service.js
@@ -12,10 +12,12 @@ var store = require('seneca-postgresql-store');
 var dgram = require('dgram');
 var service = 'cp-events-service';
 var sanitizeHtml = require('sanitize-html');
-var log = require('cp-logs-lib')({name: service, level: 'warn'});
+var log = require('cp-logs-lib')({ name: service, level: 'warn' });
 config.log = log.log;
 
-seneca.log.info('using config', JSON.stringify(config, null, 4));
+if (process.env.NODE_ENV === 'development') {
+  seneca.log.info('using config', JSON.stringify(config, null, 4));
+}
 seneca.options(config);
 /**
  * TextArea fields contains user generated html.
@@ -39,26 +41,31 @@ seneca.options.sanitizeTextArea = {
 };
 seneca.decorate('customValidatorLogFormatter', require('./lib/custom-validator-log-formatter'));
 seneca.use(store, config['postgresql-store']);
-seneca.use(require('./lib/cd-events'), {logger: log.logger});
+seneca.use(require('./lib/cd-events'), { logger: log.logger });
 seneca.use(require('cp-permissions-plugin'), {
   config: __dirname + '/config/permissions'
 });
 
 seneca.use(require('seneca-queue'));
 seneca.use(require('seneca-kue'));
-seneca.use(require('./lib/queues'), {config: config.kue});
+seneca.use(require('./lib/queues'), { config: config.kue });
+
 process.on('SIGINT', shutdown);
 process.on('SIGTERM', shutdown);
 process.on('uncaughtException', shutdown);
 process.on('SIGUSR2', shutdown);
 
 function shutdown (err) {
-  var stopQueue = seneca.export('queues/queue')['stopQueue'];
+  var stopQueue = seneca.export('queues/queue').stopQueue;
   stopQueue();
-  if (err !== void 0 && err.stack !== void 0) {
-    console.error(new Date().toString() + ' FATAL: UncaughtException, please report: ' + util.inspect(err));
-    console.error(util.inspect(err.stack));
-    console.trace();
+  if (err !== undefined && err.stack !== undefined) {
+    var error = {
+      date: new Date().toString(),
+      msg: 'FATAL: UncaughtException, please report: ' + util.inspect(err.stack),
+      err: util.inspect(err)
+    };
+    console.error(JSON.stringify(error));
+    process.exit(1);
   }
   process.exit(0);
 }
@@ -72,28 +79,32 @@ require('./migrate-psql-db.js')(function (err) {
 
   require('./network')(seneca);
 
-  seneca.ready(function (err) {
+  seneca.ready(function(err) {
     if (err) return shutdown(err);
     var message = new Buffer(service);
     var client = dgram.createSocket('udp4');
-    client.send(message, 0, message.length, 11404, 'localhost', function (err, bytes) {
+    client.send(message, 0, message.length, 11404, 'localhost', function(err, bytes) {
       if (err) return shutdown(err);
       client.close();
     });
 
     var escape = require('seneca-postgresql-store/lib/relational-util').escapeStr;
-    ['load', 'list'].forEach(function (cmd) {
-      seneca.wrap('role: entity, cmd: ' + cmd, function filterFields (args, cb) {
+    ['load', 'list'].forEach(function(cmd) {
+      seneca.wrap('role: entity, cmd: ' + cmd, function filterFields(args, cb) {
         try {
-          ['limit$', 'skip$'].forEach(function (field) {
-            if (args.q[field] && args.q[field] !== 'NULL' && !/^[0-9]+$/g.test(args.q[field] + '')) {
+          ['limit$', 'skip$'].forEach(function(field) {
+            if (
+              args.q[field] &&
+              args.q[field] !== 'NULL' &&
+              !/^[0-9]+$/g.test(args.q[field] + '')
+            ) {
               throw new Error('Expect limit$, skip$ to be a number');
             }
           });
           if (args.q.sort$) {
             if (args.q.sort$ && typeof args.q.sort$ === 'object') {
               var order = args.q.sort$;
-              _.each(order, function (ascdesc, column) {
+              _.each(order, function(ascdesc, column) {
                 if (!/^[a-zA-Z0-9_]+$/g.test(column)) {
                   throw new Error('Unexpect characters in sort$');
                 }
@@ -103,8 +114,8 @@ require('./migrate-psql-db.js')(function (err) {
             }
           }
           if (args.q.fields$) {
-            args.q.fields$.forEach(function (field, index) {
-              args.q.fields$[index] = '\"' + escape(field) + '\"';
+            args.q.fields$.forEach(function(field, index) {
+              args.q.fields$[index] = '"' + escape(field) + '"';
             });
           }
           this.prior(args, cb);

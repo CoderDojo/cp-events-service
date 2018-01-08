3393bc5ee95d25e13a4177b2bec6630924613e3f
Wardormeur <wardormeur@gmail.com>
2018-01-08 09:45:22 +0000

    replace npm w/ yarn due to failure to install packages on alpine (#178)
    * replace npm w/ yarn due to failure to install packages on alpine
    
    * add circle-ci
    
    * Change checksum based on yarn.lock instead of package.json
    
    * Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,11 +3,11 @@ set -e
 cd /usr/src/app || exit
 touch .pkg.sha1
 OLD_SHA=$(cat .pkg.sha1)
-NEW_SHA=$(sha1sum package.json)
+NEW_SHA=$(sha1sum yarn.lock)
 if [ "$OLD_SHA" != "$NEW_SHA" ]; then
   echo "$NEW_SHA" > .pkg.sha1
-  npm install
+  yarn install
   rm -rf node_modules/cp-translations
   ln -s /usr/src/cp-translations node_modules/cp-translations
 fi
-npm run dev
+yarn dev
